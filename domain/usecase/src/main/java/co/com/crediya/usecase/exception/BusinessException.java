987252aa55c414215a9331e2c4cc321641256804
Lina María <linaramirezm11@gmail.com>
2025-08-26 12:10:11 -0500

    Add BusinessException class for handling business logic errors

---
+++ domain/usecase/src/main/java/co/com/crediya/usecase/exception/BusinessException.java
@@ -0,0 +1,36 @@
+package co.com.crediya.usecase.exception;
+
+import lombok.Getter;
+
+public class BusinessException extends RuntimeException {
+
+    public enum Type{
+        EMAIL_ALREADY_EXISTS("Ya existe una cuenta asociada al correo electrónico: %s.");
+        private final String message;
+
+        public BusinessException build(){
+            return new BusinessException(this);
+        }
+
+        Type(String message) {
+            this.message = message;
+        }
+    }
+
+    private final BusinessException.Type type;
+    @Getter
+    private String personalizedMessage;
+
+
+    private BusinessException(BusinessException.Type type){
+        super(type.message);
+        this.type = type;
+    }
+
+    public BusinessException(BusinessException.Type type, String personalizedMessage){
+        super(type.message);
+        this.type = type;
+        this.personalizedMessage = String.format(type.message, personalizedMessage);
+    }
+
+}
