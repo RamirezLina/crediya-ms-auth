a80acdff5316519d549aeda5964db2086e77402c
Lina María <linaramirezm11@gmail.com>
2025-08-27 21:19:19 -0500

    [test] Add unit tests for BusinessException and DatabaseException

---
+++ domain/model/src/test/java/co/com/crediya/model/error/BusinessExcTest.java
@@ -0,0 +1,22 @@
+package co.com.crediya.model.error;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BusinessExcTest {
+
+    @Test
+    void testBuildFromType()  {
+        BusinessException ex = BusinessException.Type.EMAIL_ALREADY_EXISTS.build();
+        assertEquals("Ya existe una cuenta asociada al correo electronico o numero de identificacion", ex.getMessage());
+    }
+
+
+    @Test
+    void testConstructorWithTypeAndMessage() {
+        BusinessException ex = new BusinessException(BusinessException.Type.EMAIL_ALREADY_EXISTS, "Mensaje personalizado");
+        assertEquals("Mensaje personalizado", ex.getMessage());
+    }
+
+}
